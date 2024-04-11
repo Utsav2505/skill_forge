940a24629b7c4892ac7ef94e881b44600fbfa22c
Utsav <utsav2005maji@gmail.com>
2024-04-11 11:02:48 +0530

    all set , creating profile button

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import "@styles/globals.css";
 
 const Links = () => {
-  return <div>Links</div>;
+  return (
+    <div
+      className="linkbox"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <div className="pageLink">
+        <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+          Job Search
+        </Link>
+      </div>
+      <div className="pageLink">
+        <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+          Score Resume
+        </Link>
+      </div>
+      <div className="pageLink">
+        <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+          ask Zen
+        </Link>
+      </div>
+      <div className="pageLink">
+        <Link href="/" style={{ textDecoration: "none", color: "white" }}>
+          Mock Interview
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Links;
---
+++ components/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileButton = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default ProfileButton;

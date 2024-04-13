bfa43986b4dd2dba58f72ad671cedaeaf6b4b60f
Utsav Maji <utsav2005maji@gmail.com>
2024-04-13 10:03:39 +0530

    added url

--- app/chat-zen/page.jsx
+++ app/chat-zen/page.jsx
@@ -27,7 +27,7 @@ const Chat = () => {
     ]);
     setLoading(true);
     // Call API and add response to chat messages
-    const apiUrl = `https://dc45-34-30-227-133.ngrok-free.app/chat?question=${chatInput}`;
+    const apiUrl = `https://4c6a-34-30-227-133.ngrok-free.app/chat?question=${chatInput}`;
     const response = await fetch(apiUrl, {
       method: "POST",
       headers: {
@@ -68,7 +68,7 @@ const Chat = () => {
 
       try {
         const response = await fetch(
-          "https://dc45-34-30-227-133.ngrok-free.app/upload",
+          "https://4c6a-34-30-227-133.ngrok-free.app/upload",
           {
             method: "POST",
             body: formData,
